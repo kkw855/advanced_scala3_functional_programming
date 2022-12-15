2b7dbef6bae4aa22f3b8eae45a96fd78ae643fd1
kkw855 <kkw855@gmail.com>
2022-12-15 10:27:37 +0900

    Studying

--- src/main/scala/com/rockthejvm/part1as/AdvancedPatternMatching.scala
+++ src/main/scala/com/rockthejvm/part1as/AdvancedPatternMatching.scala
@@ -39,8 +39,86 @@ object AdvancedPatternMatching {
     case Person(status) => s"Daniel's legal drinking status is $status"
   }
 
+  // boolean patterns
+  object even {
+    def unapply(arg: Int): Boolean = arg % 2 == 0
+  }
+
+  object singleDigit {
+    def unapply(arg: Int): Boolean = arg > -10 && arg < 10
+  }
+
+  val n: Int = 42
+  val mathProperty = n match {
+    case even()        => "an even number"
+    case singleDigit() => "a one digit number"
+    case _             => "no special property"
+  }
+
+  // infix patterns
+  infix case class Or[A, B](a: A, b: B)
+  val anEither = Or(2, "two")
+  val humanDescriptionEither = anEither match {
+    case number Or string => s"$number is written as $string"
+  }
+
+  val aList = List(1, 2, 3)
+  val listPM = aList match {
+    case 1 :: rest => "a list starting with 1"
+    case _         => "some uninteresting list"
+  }
+
+  // decomposing sequences
+  val vararg = aList match {
+    case List(1, _*) => "list starting with 1"
+    case _           => "some other list"
+  }
+
+  abstract class MyList[A] {
+    def head: A = throw new NoSuchElementException
+    def tail: MyList[A] = throw new NoSuchElementException
+  }
+  case class Empty[A]() extends MyList[A]
+  case class Cons[A](override val head: A, override val tail: MyList[A])
+      extends MyList[A]
+
+  object MyList {
+    def unapplySeq[A](list: MyList[A]): Option[Seq[A]] =
+      if (list == Empty()) Some(Seq.empty)
+      else
+        unapplySeq(list.tail).map(restOfSequence => list.head +: restOfSequence)
+  }
+
+  val myList: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty())))
+  val varargCustom = myList match {
+    case MyList(1, _*) => "list starting with 1"
+    case _             => "some other list"
+  }
+
+  // custom return type for unapply
+  abstract class Wrapper[T] {
+    def isEmpty: Boolean
+    def get: T
+  }
+
+  // 99%, you should probably use options
+  object PersonWrapper {
+    def unapply(person: Person): Wrapper[String] = new Wrapper[String] {
+      override def isEmpty: Boolean = false
+      override def get: String = person.name
+    }
+  }
+
+  val weirdPersonPM = daniel match {
+    case PersonWrapper(name) => s"Hey my name is $name"
+    case _                   => "I don't know!"
+  }
+
   def main(args: Array[String]): Unit = {
     println(danielPM)
     println(danielLegalStatus)
+    println(mathProperty)
+    println(humanDescriptionEither)
+    println(weirdPersonPM)
   }
 }
